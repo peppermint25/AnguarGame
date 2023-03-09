1abc335f86fff8ba1b610b749fead4b029ee6070
peppermint25 <oskars.metr01@gmail.com>
2023-03-09 22:54:43 +0200

    added movement and some more things

--- src/app/my-game/my-game.component.ts
+++ src/app/my-game/my-game.component.ts
@@ -13,11 +13,34 @@ export class MyGameComponent {
   SpeedX = 0;
   SpeedY = 0;
 
-  plane = document.getElementById('plane')?.getBoundingClientRect;
-  airport = document.getElementById('airport');
+  // if(airport){
+  //   var airport_size = document.getElementById('airport')?.getBoundingClientRect;
+  // }
+  // airport_size = document.getElementById('airport')?.getBoundingClientRect;
+
+  airport_height = 0;
+  airport_width = 0;
+  plane_height = 0;
+  plane_width = 0;
 
 
+  gamelaunchtoken = 1;
 
+  // if(this.gamelaunchtoken == 1){
+
+  // }
+
+  startgame(){
+    var plane = document.getElementById('plane') as HTMLElement;
+    var airport = document.getElementById('airport') as HTMLElement;
+    var plane_size = plane.getBoundingClientRect();
+    var airport_size = airport.getBoundingClientRect();
+    this.airport_height  = airport_size.height;
+    this.airport_width = airport_size.width;
+    this.plane_height = plane_size.height;
+    this.plane_width = plane_size.width;
+
+  }
 
   gameplay = false;
 
@@ -42,6 +65,7 @@ export class MyGameComponent {
     this.starttime = new Date();
     console.log("starttime");
     this.playgame();
+    this.gameplay_movement();
     var result = this.create_maps();
     var map = result.selectedMap;
     var mapnumbered = result.mapnumber;
@@ -54,14 +78,60 @@ export class MyGameComponent {
         var newtime = new Date();
         var time = (newtime.getTime() - this.starttime.getTime())/1000;
         console.log(time, "laiks");
+        console.log(this.leftPress, "leftPress", this.rightPress, "rightPress")
       }, 50)
       }
     
+  };
+
+
+  plane = document.querySelector("#plane") as HTMLElement;
+
+  gameplay_movement(){
+    setInterval(() => {
+      console.log("gameplay_movement");
+      this.plane = document.querySelector("#plane") as HTMLElement;
+      this.plane.style.top = `${this.PlaneY}px`;
+      this.plane.style.left = `${this.PlaneX}px`;
+
+      this.PlaneY -= this.SpeedY;
+      this.PlaneX += this.SpeedX;
+
+      this.plane.style.top = `${this.PlaneY}px`;
+      this.plane.style.left = `${this.PlaneX}px`;
+      
+      
+      this.SpeedY = -1;
+
+      this.SpeedX = Math.min(this.SpeedX, 30);
+      this.SpeedX = Math.max(this.SpeedX, -30);
+      // this.SpeedY = Math.max(this.SpeedY, 30);
+      
+    }, 50)
+    
   }
 
+  
 
+
+
+  changeImage(): void {
+  const planeImage = document.getElementById("plane") as HTMLImageElement;
+    if (planeImage.src.endsWith("plane-right.png")) {
+      planeImage.src = "plane-left.png";
+    } else {
+      planeImage.src = "plane-right.png";
+    }
+  }
   
-  
+  changeImage1(): void {
+    const planeImage = document.getElementById("plane") as HTMLImageElement;
+    if (planeImage.src.endsWith("plane-left.png")) {
+      planeImage.src = "plane-right.png";
+    } else {
+      planeImage.src = "plane-left.png";
+    }
+  }
 
 
   // time = new Date();
@@ -109,40 +179,47 @@ export class MyGameComponent {
     return {selectedMap:selectedMap, mapnumber:mapnumber};
   };
 
+  // all movemnt and key detection functions + image change functions
+
   ngOnInit(): void {
     window.addEventListener("keydown", this.onKeyDown.bind(this));
     window.addEventListener("keyup", this.onKeyUp.bind(this));
   }
 
-
-
-
   keysPressed: {[key: number]: boolean} = {};
-  rightPress = false;
-  leftPress = false;
+  rightPress: boolean = false;
+  leftPress: boolean = false;
 
 
-onKeyDown(event: KeyboardEvent): void {
-  this.keysPressed[event.keyCode] = true;
-  if (event.keyCode === 37) {
-    console.log("Left arrow key pressed");
-  } else if (event.keyCode === 38) {
-    console.log("Up arrow key pressed");
-  } else if (event.keyCode === 39) {
-    console.log("Right arrow key pressed");
+  onKeyDown(event: KeyboardEvent): void {
+    this.keysPressed[event.keyCode] = true;
+    if (event.keyCode === 37) {
+      this.leftPress = true;
+      this.SpeedX += -15;
+      this.changeImage;
+      console.log("Left arrow key pressed");
+    } else if (event.keyCode === 38) {
+      this.SpeedY += 15;
+      console.log("Up arrow key pressed");
+    } else if (event.keyCode === 39) {
+      this.SpeedX += 15;
+      this.rightPress = true;
+      console.log("Right arrow key pressed");
+    }
+    event.preventDefault();
   }
-  event.preventDefault();
-}
 
-onKeyUp(event: KeyboardEvent): void {
-  delete this.keysPressed[event.keyCode];
-  if (event.keyCode === 37) {
-    console.log("Left arrow key released");
-  } else if (event.keyCode === 38) {
-    console.log("Up arrow key released");
-  } else if (event.keyCode === 39) {
-    console.log("Right arrow key released");
+  onKeyUp(event: KeyboardEvent): void {
+    delete this.keysPressed[event.keyCode];
+    if (event.keyCode === 37) {
+      console.log("Left arrow key released");
+      this.leftPress = false;
+    } else if (event.keyCode === 38) {
+      console.log("Up arrow key released");
+    } else if (event.keyCode === 39) {
+      this.rightPress = false;
+      console.log("Right arrow key released");
+    }
   }
-}
 
 }
