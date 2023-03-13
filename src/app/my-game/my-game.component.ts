5655ce29fb651473968e84ff739c5db5477aae7a
peppermint25 <oskars.metr01@gmail.com>
2023-03-13 09:10:15 +0200

    continueing movement changes

--- src/app/my-game/my-game.component.ts
+++ src/app/my-game/my-game.component.ts
@@ -7,39 +7,52 @@ import { Component } from '@angular/core';
 })
 export class MyGameComponent {
   name = 'My Game';
-  a =4;
-  PlaneX = 300;
-  PlaneY = 600;
-  SpeedX = 0;
-  SpeedY = 0;
-
-  // if(airport){
-  //   var airport_size = document.getElementById('airport')?.getBoundingClientRect;
-  // }
-  // airport_size = document.getElementById('airport')?.getBoundingClientRect;
 
   airport_height = 0;
   airport_width = 0;
   plane_height = 0;
   plane_width = 0;
+  maxPlaneY = 0;
+  maxPlaneX = 0;
+  gamelaunchtoken : number;
 
+  constructor(){
+    this.gamelaunchtoken = 1;
+    document.addEventListener("DOMContentLoaded", () => {
+      if (this.gamelaunchtoken == 1) {
+        this.startgame();
+      }
+    });
+  }
 
-  gamelaunchtoken = 1;
-
-  // if(this.gamelaunchtoken == 1){
-
-  // }
+  PlaneX = 0.3*this.airport_width;
+  PlaneY = 0.8*this.airport_height;
+  SpeedX = 0;
+  SpeedY = 0;
 
   startgame(){
     var plane = document.getElementById('plane') as HTMLElement;
     var airport = document.getElementById('airport') as HTMLElement;
+    // var plane_area = document.getElementById('plane_area') as HTMLElement;
+    if(!plane || !airport){
+      console.error('Can not find plane or airport');
+      return;
+    }
+
     var plane_size = plane.getBoundingClientRect();
     var airport_size = airport.getBoundingClientRect();
+    console.log("airport", airport_size, "plane", plane_size);
+
+    
+
     this.airport_height  = airport_size.height;
     this.airport_width = airport_size.width;
     this.plane_height = plane_size.height;
     this.plane_width = plane_size.width;
-
+    this.maxPlaneX = this.airport_width - this.plane_width;
+    this.maxPlaneY = this.airport_height - this.plane_height;
+    // console.log("Plane" , plane_size, plane, plane.style)
+    console.log("maxPlaneX: " + this.maxPlaneX, "Plane width: ", this.plane_width, "Airport width: ", this.airport_width);
   }
 
   gameplay = false;
@@ -63,7 +76,6 @@ export class MyGameComponent {
   StartGame() {
     this.gameplay = true;
     this.starttime = new Date();
-    console.log("starttime");
     this.playgame();
     this.gameplay_movement();
     var result = this.create_maps();
@@ -77,8 +89,9 @@ export class MyGameComponent {
       setInterval(() => {
         var newtime = new Date();
         var time = (newtime.getTime() - this.starttime.getTime())/1000;
-        console.log(time, "laiks");
-        console.log(this.leftPress, "leftPress", this.rightPress, "rightPress")
+        // console.log(time, "laiks");
+        // console.log(this.PlaneY, this.maxPlaneY)
+        // console.log(this.PlaneX, this.maxPlaneX, this.plane_width, this.PlaneX+this.plane_width)
       }, 50)
       }
     
@@ -89,7 +102,6 @@ export class MyGameComponent {
 
   gameplay_movement(){
     setInterval(() => {
-      console.log("gameplay_movement");
       this.plane = document.querySelector("#plane") as HTMLElement;
       this.plane.style.top = `${this.PlaneY}px`;
       this.plane.style.left = `${this.PlaneX}px`;
@@ -103,24 +115,52 @@ export class MyGameComponent {
       
       this.SpeedY = -1;
 
+
+
+      if(this.PlaneY >= this.maxPlaneY){
+        this.SpeedY =+ 1.5;
+      }else if(this.PlaneY <= 0){
+        this.SpeedY = -0.1*this.maxPlaneY;
+      }
+
+      if(this.leftPress == true){
+        console.log("i changed left press");
+        this.changeImage;
+      }
+
+      if(this.rightPress == true){
+        console.log("i changed right press");
+        this.changeImage1;
+      }
+
       this.SpeedX = Math.min(this.SpeedX, 30);
       this.SpeedX = Math.max(this.SpeedX, -30);
       // this.SpeedY = Math.max(this.SpeedY, 30);
-      
-    }, 50)
+      // console.log("airport width", this.airport_width, "airport height", this.airport_height);
+      this.PlaneX = Math.min(this.PlaneX, this.maxPlaneX);
+      this.PlaneX = Math.max(this.PlaneX, 0);
+      this.PlaneY = Math.max(this.PlaneY, 0);
+      this.PlaneY = Math.min(this.PlaneY, this.maxPlaneY);
+
+    }, 30)
     
   }
 
-  
+  drawEnemy(x : number, y : number){
+
+  }
 
 
 
   changeImage(): void {
   const planeImage = document.getElementById("plane") as HTMLImageElement;
-    if (planeImage.src.endsWith("plane-right.png")) {
-      planeImage.src = "plane-left.png";
+
+  const backgroundImageURL = getComputedStyle(planeImage).backgroundImage.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
+
+    if (backgroundImageURL.endsWith("plane-right.png")) {
+      planeImage.style.backgroundImage = "url('plane-left.png')";
     } else {
-      planeImage.src = "plane-right.png";
+      planeImage.style.backgroundImage = "url('plane-right.png')";
     }
   }
   
@@ -197,28 +237,22 @@ export class MyGameComponent {
       this.leftPress = true;
       this.SpeedX += -15;
       this.changeImage;
-      console.log("Left arrow key pressed");
     } else if (event.keyCode === 38) {
       this.SpeedY += 15;
-      console.log("Up arrow key pressed");
     } else if (event.keyCode === 39) {
       this.SpeedX += 15;
       this.rightPress = true;
-      console.log("Right arrow key pressed");
     }
     event.preventDefault();
   }
 
   onKeyUp(event: KeyboardEvent): void {
     delete this.keysPressed[event.keyCode];
-    if (event.keyCode === 37) {
-      console.log("Left arrow key released");
+    if (event.keyCode === 37) {   
       this.leftPress = false;
     } else if (event.keyCode === 38) {
-      console.log("Up arrow key released");
     } else if (event.keyCode === 39) {
       this.rightPress = false;
-      console.log("Right arrow key released");
     }
   }
 
